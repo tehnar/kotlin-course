132c90cc1aeae2e5587356e47bf7c46c9a91d95f
Vsevolod Stepanov <tehnar5@gmail.com>
2017-10-28 22:12:46 +0300

    Solve 701E CF problem

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,121 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class Vertex {
+    var hasUniversity: Boolean = false
+    var universitiesInSubtree: Int = 0
 }
 
+class Graph {
+    private val edges: MutableMap<Vertex, MutableList<Vertex>> = HashMap()
+
+    fun addEdge(from: Vertex, to: Vertex) {
+        edges.getOrPut(from, { mutableListOf() }).add(to)
+        edges.getOrPut(to, { mutableListOf() }).add(from)
+    }
+
+    fun getNeighbours(from: Vertex): List<Vertex> = edges[from] ?: listOf()
+}
+
+class Solver {
+
+    fun solve(graph: Graph, root: Vertex, universitiesPairCount: Int): Long {
+        calcUniversitiesInSubtrees(graph, root)
+        val optimalRoot = findOptimalRoot(graph, root, universitiesPairCount)
+        return calcSumOfDistancesToRoot(graph, optimalRoot)
+    }
+
+    private fun calcSumOfDistancesToRoot(graph: Graph, root: Vertex): Long =
+            calcSumOfDistancesToRoot(graph, root, null, 0)
+
+    private fun calcSumOfDistancesToRoot(graph: Graph, curVertex: Vertex,
+                                         parent: Vertex?, distToRoot: Int): Long {
+        val sumInChildren = graph.getNeighbours(curVertex).filter { it != parent }.map {
+            calcSumOfDistancesToRoot(graph, it, curVertex, distToRoot + 1)
+        }.sum()
+
+        return if (curVertex.hasUniversity) sumInChildren + distToRoot else sumInChildren
+    }
+
+    private fun calcUniversitiesInSubtrees(graph: Graph, root: Vertex) {
+        calcUniversitiesInSubtrees(graph, root, null)
+    }
+
+    private fun calcUniversitiesInSubtrees(graph: Graph, curVertex: Vertex, parent: Vertex?): Int {
+        val childCount = graph.getNeighbours(curVertex).filter { it != parent }.map {
+            calcUniversitiesInSubtrees(graph, it, curVertex)
+        }.sum()
+
+        val childCountAndMe = if (curVertex.hasUniversity) childCount + 1 else childCount
+        curVertex.universitiesInSubtree = childCountAndMe
+        return childCountAndMe
+    }
+
+    private fun findOptimalRoot(graph: Graph, root: Vertex, universitiesPairCount: Int): Vertex =
+            findOptimalRoot(graph, root, null, 0, universitiesPairCount)!!
+
+    private fun findOptimalRoot(graph: Graph, curVertex: Vertex, parent: Vertex?, universitiesAbove: Int,
+                                universitiesPairCount: Int): Vertex? {
+        val children = graph.getNeighbours(curVertex).filter { it != parent }
+        if (children.isEmpty()) {
+            return if (universitiesAbove <= universitiesPairCount) curVertex else null
+        }
+        
+        val vertexWithMaxUniversitiesInSubtree = children.maxBy { it.universitiesInSubtree }!!
+        val maxUniversitiesCount = vertexWithMaxUniversitiesInSubtree.universitiesInSubtree
+
+        return if (Math.max(maxUniversitiesCount, universitiesAbove) <= universitiesPairCount ) {
+            curVertex
+        } else {
+            children.mapNotNull {
+                findOptimalRoot(graph, it, curVertex,
+                        universitiesAbove + curVertex.universitiesInSubtree - it.universitiesInSubtree,
+                        universitiesPairCount)
+            }.firstOrNull()
+        }
+    }
+}
+
+class DataReader(stream: InputStream) {
+    private val reader = BufferedReader(InputStreamReader(stream))
+    private var st : StringTokenizer? = null
+
+    private fun next() : String? {
+        while (st == null || !st!!.hasMoreTokens()) {
+            val s = reader.readLine() ?: return null
+            st = StringTokenizer(s)
+        }
+
+        return st?.nextToken()
+    }
+
+    private fun nextToken() = next()!!
+
+    fun nextInt() = nextToken().toInt()
+}
+
+
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val reader = DataReader(System.`in`)
+    val vertexCount = reader.nextInt()
+    val universityPairCount = reader.nextInt()
+
+    val graph = Graph()
+    val vertices = List(vertexCount, { Vertex() })
+
+    for (i in 0 until 2 * universityPairCount) {
+        val id = reader.nextInt()
+        vertices[id - 1].hasUniversity = true
+    }
+    for (i in 0 until vertexCount - 1) {
+        val from = reader.nextInt()
+        val to = reader.nextInt()
+        graph.addEdge(vertices[from - 1], vertices[to - 1])
+    }
+
+    println(Solver().solve(graph, vertices[0], universityPairCount))
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -4,7 +4,48 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSample1() {
+        val vertexCount = 7
+        val universityPairCount = 2
+        val vertices = List(vertexCount, { Vertex() })
+        val graph = Graph()
+        graph.addEdge(vertices[0], vertices[2])
+        graph.addEdge(vertices[2], vertices[1])
+        graph.addEdge(vertices[3], vertices[4])
+        graph.addEdge(vertices[2], vertices[6])
+        graph.addEdge(vertices[3], vertices[2])
+        graph.addEdge(vertices[3], vertices[5])
+
+        vertices[0].hasUniversity = true
+        vertices[4].hasUniversity = true
+        vertices[5].hasUniversity = true
+        vertices[1].hasUniversity = true
+
+        assertEquals(6, Solver().solve(graph, vertices[0], universityPairCount))
+    }
+
+    @Test
+    fun testSample2() {
+        val vertexCount = 9
+        val universityPairCount = 3
+        val vertices = List(vertexCount, { Vertex() })
+        val graph = Graph()
+        graph.addEdge(vertices[7], vertices[8])
+        graph.addEdge(vertices[2], vertices[1])
+        graph.addEdge(vertices[1], vertices[6])
+        graph.addEdge(vertices[2], vertices[3])
+        graph.addEdge(vertices[6], vertices[5])
+        graph.addEdge(vertices[3], vertices[4])
+        graph.addEdge(vertices[1], vertices[0])
+        graph.addEdge(vertices[1], vertices[7])
+
+        vertices[2].hasUniversity = true
+        vertices[1].hasUniversity = true
+        vertices[0].hasUniversity = true
+        vertices[5].hasUniversity = true
+        vertices[4].hasUniversity = true
+        vertices[8].hasUniversity = true
+
+        assertEquals(9, Solver().solve(graph, vertices[0], universityPairCount))
     }
 }
